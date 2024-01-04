cb8ad47a245328517edf17fe306bc599c9f43e9a
ssung2sin <ssung2sin@naver.com>
2024-01-05 03:39:38 +0900

    [백준]1018/40분/정답(다시품1018_2)

--- ssung2sin/1 weeek/A1018_1.java
+++ ssung2sin/1 weeek/A1018_1.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class A1018 {
+public class A1018_1 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
