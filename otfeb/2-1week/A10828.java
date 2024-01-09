0d8c748b5cd4860fc4d7a8f8125645233bba87e7
HoSeok <hs970216@naver.com>
2024-01-09 22:22:35 +0900

    [백준]10828/20분/정답

--- otfeb/2-1week/A10828.java
+++ otfeb/2-1week/A10828.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Stack;
 
-public class bj_10828 {
+public class A10828 {
 
    public static void main(String[] args) throws IOException {
       
